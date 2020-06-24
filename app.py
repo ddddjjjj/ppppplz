d9b6fdf6b7fe098753331a429020e9488cbe3034
ddddjjjj <43689301+ddddjjjj@users.noreply.github.com>
2020-06-24 17:35:23 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from __future__ import with_statement
+import time
+from sqlite3 import dbapi2 as sqlite3
+from hashlib import md5
+from datetime import datetime
+from contextlib import closing
+from flask import Flask, request, session, url_for, redirect, \
+     render_template, abort, g, flash
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# configuration
+DATABASE = 'minitwit.db'
+PER_PAGE = 30
+DEBUG = True
+SECRET_KEY = 'development key'
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('MINITWIT_SETTINGS', silent=True)
+
+
+@app.route('/')
+def hello():
+    return render_template('html.html')
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', debug=True)
